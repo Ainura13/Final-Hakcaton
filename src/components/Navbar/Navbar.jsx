e0f73deddbb7391b3802617c61dcef6062988877
nurjan1501 <babyrabbit1501@gmail.com>
2022-05-25 20:29:28 +0600

    nurjan

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -59,142 +59,152 @@ const Navbar = () => {
   }, [addProductToCart]);
 
   return (
-    <AppBar position="static" elevation={3}>
-      <Container maxWidth="xl" sx={{ background: '#eeb956' }}>
-        <Toolbar disableGutters>
-          <Link to="/" mr={4}>
-            <img
-              src="https://cassandralavalle.com/wp-content/uploads/2021/10/site-logo.svg"
-              alt="123"
-              width={250}
-              height={100}
-            />
-          </Link>
+    <Box
+      sx={{
+        background: '#F5F3EE;',
+        flexWrap: { xs: 'wrap', sm: 'wrap' },
+        width: '100%',
+      }}
+    >
+      <AppBar position="static" elevation={3}>
+        <Container maxWidth="xl" sx={{ background: '#eeb956' }}>
+          <Toolbar disableGutters>
+            <Link to="/" mr={4}>
+              <img
+                src="https://cassandralavalle.com/wp-content/uploads/2021/10/site-logo.svg"
+                alt="123"
+                width={250}
+                height={100}
+              />
+            </Link>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                  color: 'black',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                <Box>
+                  {pages.map((page) => (
+                    <MenuItem key={page.id} onClick={handleCloseNavMenu}>
+                      <Link to={page.link}>
+                        <Typography
+                          sx={{
+                            ml: 'auto',
+                            my: 1,
+                            color: 'black',
+                            display: 'block',
+                          }}
+                        >
+                          {page.name}
+                        </Typography>
+                      </Link>
+                    </MenuItem>
+                  ))}
+                </Box>
+              </Menu>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-                color: 'black',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+              <img
+                id="logo"
+                src="https://demo.xpeedstudio.com/marketo/wp-content/uploads/2020/06/logo_3.png"
+                alt=""
+              />
+            </Typography>
+            <Box
               sx={{
-                display: { xs: 'block', md: 'none' },
+                justifyContent: 'center',
+                flexGrow: 1,
+                display: { xs: 'none', md: 'flex' },
               }}
             >
-              <Box>
-                {pages.map((page) => (
-                  <MenuItem key={page.id} onClick={handleCloseNavMenu}>
-                    <Link to={page.link}>
-                      <Typography
-                        sx={{
-                          ml: 'auto',
-                          my: 1,
-                          color: 'black',
-                          display: 'block',
-                        }}
-                      >
-                        {page.name}
-                      </Typography>
-                    </Link>
-                  </MenuItem>
-                ))}
-              </Box>
-            </Menu>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-          >
-            <img
-              id="logo"
-              src="https://demo.xpeedstudio.com/marketo/wp-content/uploads/2020/06/logo_3.png"
-              alt=""
-            />
-          </Typography>
-          <Box
-            sx={{
-              justifyContent: 'center',
-              flexGrow: 1,
-              display: { xs: 'none', md: 'flex' },
-            }}
-          >
-            {pages.map((page) => (
-              <Link to={page.link} key={page.id}>
-                <Button
-                  onClick={handleCloseNavMenu}
-                  sx={{
-                    ml: 'auto',
-                    my: 2,
-                    color: 'black',
-                    display: 'block',
-                    fontWeight: 'bold',
-                  }}
+              {pages.map((page) => (
+                <Link to={page.link} key={page.id}>
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{
+                      ml: 'auto',
+                      my: 2,
+                      color: 'black',
+                      display: 'block',
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    {page.name}
+                  </Button>
+                </Link>
+              ))}
+            </Box>
+
+            <Link to="/favourit">
+              <Button sx={{ my: 2, color: 'black' }}>
+                <Badge
+                  badgeContent={count}
+                  color="primary"
+                  sx={{ mr: '10px', mt: '5px' }}
                 >
-                  {page.name}
-                </Button>
-              </Link>
-            ))}
-          </Box>
+                  <FavoriteIcon
+                    sx={{ fontSize: '30px', mt: '5px', mr: '5px' }}
+                  />
+                </Badge>
+              </Button>
+            </Link>
 
-          <Link to="/favourit">
-            <Button sx={{ my: 2, color: 'black' }}>
-              <Badge
-                badgeContent={count}
-                color="primary"
-                sx={{ mr: '10px', mt: '5px' }}
-              >
-                <FavoriteIcon sx={{ fontSize: '30px', mt: '5px', mr: '5px' }} />
-              </Badge>
-            </Button>
-          </Link>
+            <Link to="/cart">
+              <Button sx={{ my: 2, color: 'black' }}>
+                <Badge
+                  badgeContent={count}
+                  color="primary"
+                  sx={{ mr: '10px', mt: '5px' }}
+                >
+                  <ShoppingCartIcon sx={{ fontSize: '30px' }} />
+                </Badge>
+              </Button>
+            </Link>
 
-          <Link to="/cart">
-            <Button sx={{ my: 2, color: 'black' }}>
-              <Badge
-                badgeContent={count}
-                color="primary"
-                sx={{ mr: '10px', mt: '5px' }}
-              >
-                <ShoppingCartIcon sx={{ fontSize: '30px' }} />
-              </Badge>
-            </Button>
-          </Link>
+            <NavLink to="/login">
+              <Button color="inherit" sx={{ color: 'white' }}>
+                Login
+              </Button>
+            </NavLink>
 
-          <NavLink to="/login">
-            <Button color="inherit" sx={{ color: 'white' }}>
-              Login
-            </Button>
-          </NavLink>
-
-          <NavLink to="/register">
-            <Button color="inherit" sx={{ color: 'white' }}>
-              Register
-            </Button>
-          </NavLink>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            <NavLink to="/register">
+              <Button color="inherit" sx={{ color: 'white' }}>
+                Register
+              </Button>
+            </NavLink>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
   );
 };
 export default Navbar;
--- src/components/product/AddProduct.jsx
+++ src/components/product/AddProduct.jsx
@@ -1,20 +1,31 @@
-import { Box, Button, FormControl, Input, InputLabel, Menu, MenuItem, Select, TextField, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  FormControl,
+  Input,
+  InputLabel,
+  Menu,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useProducts } from '../../contexts/ProductContexProvider';
 
 const AddProduct = () => {
-  const { addProduct, getProducts, categorys,getCategory  } = useProducts();
+  const { addProduct, getProducts, categorys, getCategory } = useProducts();
   const navigate = useNavigate();
   useEffect(() => {
     getProducts();
-}, []);
+  }, []);
 
-useEffect(() => {
-  getCategory();
-}, []);
+  useEffect(() => {
+    getCategory();
+  }, []);
 
-console.log(categorys);
+  console.log(categorys);
   const [product, setProduct] = useState({
     name: '',
     description: '',
@@ -40,69 +51,66 @@ console.log(categorys);
     }
   };
 
-
   const handleInpFile = (e) => {
     let file = e.target.files[0];
-    setProduct({...product, image: file})
-}
+    setProduct({ ...product, image: file });
+  };
 
   return (
     <Box
-    className='bgImage' sx={{
-    height: '70vh' ,
-backgroundImage: `url("https://images.unsplash.com/photo-1524055988636-436cfa46e59e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80"), linear-gradient(#fff,#0000)`,
-    position: 'relative', 
-    color: 'white',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'right',
-    backgroundAttachment: 'fixed',
-    opacity: '0.8'}}>
-
-    <Box sx={{ width: '50vw', margin: '10vh auto' }}>
-    <Typography
-      variant='h4' 
-      align='center' 
-      color='textPrimary' 
-      gutterBottom
-      >
-        Admin Page
-      </Typography>
-      <TextField
-        fullWidth
-        id="outlined-basic"
-        label="Name"
-        variant="outlined"
-        name="name"
-        onChange={handleInp}
-        sx={{mb: 2}}
-      />
-      <TextField
-        fullWidth
-        id="outlined-basic"
-        label="Description"
-        variant="outlined"
-        name="description"
-        onChange={handleInp}
-        sx={{mb: 2}}
-      />
-      <TextField
-        fullWidth
-        id="outlined-basic"
-        label="Price"
-        variant="outlined"
-        name="price"
-        onChange={handleInp}
-        sx={{mb: 2}}
-      />
-      <Box sx={{ minWidth: 120 }}>
-      <Input
-                type='file'
-                onChange={handleInpFile}
-                sx={{mb: 2}}
-                />
-      </Box>
-      {/* {categorys ? (
+      className="bgImage"
+      sx={{
+        height: '70vh',
+        backgroundImage: `url("https://images.unsplash.com/photo-1653200256706-a5bc2f69abb7?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=829https://images.unsplash.com/photo-1652517210558-954882f97dca?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812"), linear-gradient(#fff,#0000)`,
+        position: 'relative',
+        color: 'white',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'right',
+        backgroundAttachment: 'fixed',
+        opacity: '0.8',
+      }}
+    >
+      <Box sx={{ width: '50vw', margin: '10vh auto' }}>
+        <Typography
+          variant="h4"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Admin Page
+        </Typography>
+        <TextField
+          fullWidth
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          name="name"
+          onChange={handleInp}
+          sx={{ mb: 2 }}
+        />
+        <TextField
+          fullWidth
+          id="outlined-basic"
+          label="Description"
+          variant="outlined"
+          name="description"
+          onChange={handleInp}
+          sx={{ mb: 2 }}
+        />
+        <TextField
+          fullWidth
+          id="outlined-basic"
+          label="Price"
+          variant="outlined"
+          name="price"
+          onChange={handleInp}
+          sx={{ mb: 2 }}
+        />
+        <Box sx={{ minWidth: 120 }}>
+          <Input type="file" onChange={handleInpFile} sx={{ mb: 2 }} />
+        </Box>
+        {/* {categorys ? (
             categorys.map((item) => (
               <Select
           labelId="demo-simple-select-label"
@@ -125,51 +133,50 @@ backgroundImage: `url("https://images.unsplash.com/photo-1524055988636-436cfa46e
           <MenuItem value={categorys}>No categorys</MenuItem>
         </Select>
           )} */}
-     <Box sx={{ minWidth: 120, mb: 2 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Category</InputLabel>
-      <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value=''
-          label="Age"
-          onChange={handleInp}
-        >
-          {categorys ? (
-            categorys.map((item) => (
-              <MenuItem value={item.name}>{item.name}</MenuItem>
-            ))
-          ) : (
-            <MenuItem value={null}>No category</MenuItem>
-          )}
-          
-        </Select>
-        </FormControl>
+        <Box sx={{ minWidth: 120, mb: 2 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Category</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value=""
+              label="Age"
+              onChange={handleInp}
+            >
+              {categorys ? (
+                categorys.map((item) => (
+                  <MenuItem value={item.name}>{item.name}</MenuItem>
+                ))
+              ) : (
+                <MenuItem value={null}>No category</MenuItem>
+              )}
+            </Select>
+          </FormControl>
         </Box>
-        
-      <TextField
-        fullWidth
-        id="outlined-basic"
-        label="Made_in"
-        variant="outlined"
-        name="made_in"
-        onChange={handleInp}
-        sx={{mb: 2}}
-      />
-      <Button
-        variant="outlined"
-        fullWidth
-        size="large"
-        color='success'
-        sx={{mb: 2}}
-        onClick={() => {
-          addProduct(product);
-          navigate('/products');
-        }}
-      >
-        CREATE PRODUCT
-      </Button>
-    </Box>
+
+        <TextField
+          fullWidth
+          id="outlined-basic"
+          label="Made_in"
+          variant="outlined"
+          name="made_in"
+          onChange={handleInp}
+          sx={{ mb: 2 }}
+        />
+        <Button
+          variant="outlined"
+          fullWidth
+          size="large"
+          color="success"
+          sx={{ mb: 2 }}
+          onClick={() => {
+            addProduct(product);
+            navigate('/products');
+          }}
+        >
+          CREATE PRODUCT
+        </Button>
+      </Box>
     </Box>
   );
 };
